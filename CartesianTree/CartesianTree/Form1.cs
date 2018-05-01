9d8ddc364d7bb13c93f22d3bc1be1d2b25030f79
dunvl <dunvladyur4@gmail.com>
2018-05-01 22:18:08 +0300

    Added class NodeDrawer for Draw node of something tree

--- CartesianTree/CartesianTree/Form1.cs
+++ CartesianTree/CartesianTree/Form1.cs
@@ -17,12 +17,28 @@ namespace CartesianTree
         {
             InitializeComponent();
             Random rnd = new Random();
-            for(int i= 0; i < 1000; ++i)
+            do
             {
-                treap.Add(new HeightAndSizeInfo(rnd.Next()));
-            }
-            var r = treap.Split(new HeightAndSizeInfo(1000000));
+                treap = new Treap<NodeWithHeightAndSize, HeightAndSizeInfo>();
+                for (int i = 0; i < 31; ++i)
+                {
+                    treap.Add(new HeightAndSizeInfo(rnd.Next(10000)));
+                }
+            } while (treap.Root.Info.Value.Height > 6);
+
             int asdasd = 0;
+            var qweq = new NodeDrawer()
+            {
+                Size = 92;
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = panel1.CreateGraphics();
+            Pen pen = new Pen(Color.Black, 2);
+            graphics.DrawRectangle(pen, 100, 100, 60, 60);
+            graphics.DrawString("Значение: 45\nПриоритет:12", new Font("Microsoft Sans Serif", 10), pen.Brush, 100, 100);
         }
     }
 }
---
+++ CartesianTree/CartesianTree/NodeDrawer.cs
@@ -0,0 +1,56 @@
+﻿using System.Drawing;
+
+namespace CartesianTree
+{
+    /// <summary>
+    /// Класс, позволяющий рисовать узлы деревьев
+    /// </summary>
+    class NodeDrawer
+    {
+        /// <summary>
+        /// Расстояние между уровнями в дереве
+        /// </summary>
+        public int DistanseHeight { get; set; } = 45;
+
+        /// <summary>
+        /// Высота на которой будет рисоваться узел
+        /// </summary>
+        public int StartY { get; set; } = 0;
+
+        /// <summary>
+        /// Размер узла дерева
+        /// </summary>
+        public int Size { get; set; } = 60;
+
+        /// <summary>
+        /// Начало области, в которой можно отображать узел (по ширине)
+        /// </summary>
+        public int StartX { get; set; } = 0;
+
+        /// <summary>
+        /// Конец области, в которой можно отображать узел (по ширине)
+        /// </summary>
+        public int EndX { get; set; } = 1000;
+
+        /// <summary>
+        /// Цвет границ узлов
+        /// </summary>
+        public Pen PenBorder { get; set; } = new Pen(Color.Black, 2);
+
+        /// <summary>
+        /// Шрифт для отображения информации внутри узла
+        /// </summary>
+        public Font Font { get; set; } = new Font("Calibri", 11);
+
+        /// <summary>
+        /// Цвет шрифта
+        /// </summary>
+        public Brush BrushFont { get; set; } = new SolidBrush(Color.Red);
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public NodeDrawer() { }
+
+    }
+}
