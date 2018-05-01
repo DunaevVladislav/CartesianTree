1d30dabf0972c99887a8f9136dac671d9c6d8646
dunvl <dunvladyur4@gmail.com>
2018-05-01 13:59:21 +0300

    Added class, which implement node of Cartesian tree Interface INodeInfo is replaced by an abstract class

---
+++ CartesianTree/CartesianTree/NodeInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CartesianTree
+{
+    /// <summary>
+    /// Класс, реализующий хранение информации в дереве и ее обновление
+    /// </summary>
+    /// <typeparam name="TValue">Информация, которая хранится в узле дерева</typeparam>
+    abstract class NodeInfo<TValue>:IComparable where TValue:IComparable
+    {
+        /// <summary>
+        /// Значение узла
+        /// </summary>
+        public TValue Value { get; set; }
+
+        /// <summary>
+        /// Обновление информации в узле, в соответствии с ее левым и правом сыном
+        /// </summary>
+        /// <param name="left">Левый сын</param>
+        /// <param name="right">Правый сын</param>
+        public abstract void Update(NodeInfo<TValue> left, NodeInfo<TValue> right);
+
+        /// <summary>
+        /// Компаратор
+        /// </summary>
+        /// <param name="obj">Объект, с которым производится сравение</param>
+        /// <returns>Результат сравнения</returns>
+        public int CompareTo(object obj)
+        {
+            if (obj is NodeInfo<TValue>)
+            {
+                return this.CompareTo((obj as NodeInfo<TValue>));
+            }
+            else
+            {
+                throw new Exception("Compare with the non-admissible class");
+            }
+        }
+    }
+}
---
+++ CartesianTree/CartesianTree/NodeOfCartesianTree.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace CartesianTree
+{
+    /// <summary>
+    /// Узел декартового дерева
+    /// </summary>
+    /// <typeparam name="TValue">Информация хранимая в декартовом дереве</typeparam>
+    class NodeOfCartesianTree<TValue> where TValue:IComparable
+    {
+        /// <summary>
+        /// Объект для генерации случайных величин
+        /// </summary>
+        private static Random random = new Random();
+
+        /// <summary>
+        /// Информация хранящайся в узле дерева
+        /// </summary>
+        public NodeInfo<TValue> Info { get; set; }
+
+        /// <summary>
+        /// Приоритет узла
+        /// </summary>
+        public int Priority { get; private set; }
+
+        /// <summary>
+        /// Левый сын дерева
+        /// </summary>
+        public NodeOfCartesianTree<TValue> Left { get; set; }
+
+        /// <summary>
+        /// Правый сын дерева
+        /// </summary>
+        public NodeOfCartesianTree<TValue> Right { get; set; }
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="info">Информация хранящайся в узле дерева</param>
+        /// <param name="left">Левый сын</param>
+        /// <param name="right">Правый сын</param>
+        public NodeOfCartesianTree(NodeInfo<TValue> info = null, NodeOfCartesianTree<TValue> left = null, NodeOfCartesianTree<TValue> right = null)
+        {
+            Info = info;
+            Priority = random.Next(int.MinValue, int.MaxValue);
+            Left = left;
+            Right = right;
+        }
+    }
+}
